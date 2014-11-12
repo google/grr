8a4813fcbf4eb389b3697ebbdadd79379315742a
destijl <github@mailgreg.com>
2014-11-12 05:34:00 +0000

    Add missing data_server/tests.py

---
+++ server/data_server/tests.py
@@ -0,0 +1,6 @@
+"""Data Server tests."""
+
+# These need to register plugins so, pylint: disable=unused-import
+from grr.server.data_server import auth_test
+from grr.server.data_server import master_test
+# pylint: enable=unused-import
