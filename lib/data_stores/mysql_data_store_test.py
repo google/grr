9e2deee14a693396e92b363a849edc8ecf1e2bc4
Sean Gillespie <seagill@yahoo-inc.com>
2014-12-01 15:38:13 -0800

    MySQL Datastore Enhancements

--- lib/data_stores/mysql_data_store_test.py
+++ lib/data_stores/mysql_data_store_test.py
@@ -23,14 +23,13 @@ class MysqlTestMixin(object):
     # Use separate tables for benchmarks / tests so they can be run in parallel.
     config_lib.CONFIG.Set("Mysql.database_name", "grr_test_%s" %
                           self.__class__.__name__)
-    config_lib.CONFIG.Set("Mysql.table_name", "aff4_test")
 
     data_store.DB = mysql_data_store.MySQLDataStore()
     data_store.DB.security_manager = test_lib.MockSecurityManager()
-    data_store.DB.RecreateDataBase()
+    data_store.DB.RecreateTables()
 
   def DestroyDatastore(self):
-    data_store.DB.DropDatabase()
+    data_store.DB.DropTables()
 
   def testCorrectDataStore(self):
     self.assertTrue(isinstance(data_store.DB, mysql_data_store.MySQLDataStore))
