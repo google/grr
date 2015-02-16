79bd6e4acb5bc72481ce2afb1a1b9686c4df7a5f
Ben Galehouse <bgalehouse@gmail.com>
2015-02-16 12:39:30 +0100

    Remove dependency on Google argument parsing.

--- client/minicomm/client_main.cc
+++ client/minicomm/client_main.cc
@@ -1,12 +1,12 @@
 
 #include "client.h"
 
-DEFINE_string(config_filename, "", "Where to find the config file.");
-
 int main(int argc, char* argv[]) {
-  InitGoogle(argv[0], &argc, &argv, true);
   grr::Client::StaticInit();
-  grr::Client client(FLAGS_config_filename);
+  if (argc != 2) {
+    GOOGLE_LOG(FATAL) << "Usage is: client <config>";
+  }
+  grr::Client client(argv[1]);
   client.Run();
   return 0;
 }
