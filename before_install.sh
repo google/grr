ef74925fc27537599b2fdcff05c43b948d3147c3
Dionysis Zindros <dionyziz@gmail.com>
2015-02-24 07:04:19 -0800

    Install protobufc from debs instead of apt.
    Ubuntu 12.04 has an outdated version of protobufs. We need to use
    protobuf 2.6.x and only 2.5.x is available. So I'm moving this to use
    prepackaged debs for precise.

--- before_install.sh
+++ before_install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-sudo apt-get install python-protobuf libprotoc-dev protobuf-compiler m2crypto python-support libdistorm64-1 libdistorm64-dev python-psutil pytsk3 ncurses-dev python-pip
+PROTO_PACKAGES=(libprotobuf9_2.6.1-1ppa1~precise_amd64.deb libprotobuf-lite9_2.6.1-1ppa1~precise_amd64.deb libprotobuf-dev_2.6.1-1ppa1~precise_amd64.deb libprotobuf-java_2.6.1-1ppa1~precise_all.deb libprotoc9_2.6.1-1ppa1~precise_amd64.deb libprotoc-dev_2.6.1-1ppa1~precise_amd64.deb protobuf-compiler_2.6.1-1ppa1~precise_amd64.deb python-protobuf_2.6.1-1ppa1~precise_amd64.deb)
+
+mkdir protobuf-debs
+cd protobuf-debs
+
+echo "Installing protobuf debs"
+for i in ${PROTO_PACKAGES[@]}; do
+  echo "Downloading custom package $i"
+  wget https://dionyziz.com/protobuf-debs/$i
+  echo "Installing $i"
+  sudo dpkg -i $i
+done
+cd ..
+echo "Protobuf debs installed"
+
+sudo apt-get install m2crypto python-support libdistorm64-1 libdistorm64-dev python-psutil pytsk3 ncurses-dev python-pip
 
 PLAT=amd64
 DEB_DEPENDENCIES_URL=https://googledrive.com/host/0B1wsLqFoT7i2aW5mWXNDX1NtTnc/ubuntu-12.04-${PLAT}-debs.tar.gz
