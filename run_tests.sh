29bda743419de0832fc023375e7a4eb59620ec40
Dionysis Zindros <dionyziz@gmail.com>
2015-02-24 15:26:27 -0800

    Make run_tests.sh script return exit code

--- run_tests.sh
+++ run_tests.sh
@@ -4,3 +4,5 @@ echo 'Running tests...'
 
 EXCLUDE_TESTS=HTTPDataStoreCSVBenchmarks,HTTPDataStoreBenchmarks,MicroBenchmarks,TDBDataStoreBenchmarks,FakeDataStoreBenchmarks,AverageMicroBenchmarks,SqliteDataStoreBenchmarks,DataStoreCSVBenchmarks,TDBDataStoreCSVBenchmarks,AFF4Benchmark
 PYTHONPATH=. python grr/run_tests.py --processes=1 --exclude_tests=$EXCLUDE_TESTS 2>&1|grep -v 'DEBUG:'|grep -v 'INFO:'
+TEST_STATUS=${PIPESTATUS[0]}
+exit $TEST_STATUS
