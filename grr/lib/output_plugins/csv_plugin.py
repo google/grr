faa1abf2750c031efaa6642706c4d5da2724c281
Mikhail Bushkov <realbushman@gmail.com>
2017-05-30 10:50:29 +0200

    Removed legacy CSVOuptutPlugin.
    Legacy CSVOutputPlugin was deprecated in favour of
    InstantCSVOutputPlugin (the one used via the "Download As: CSV (zipped)"
    in the UI).

--- grr/lib/output_plugins/csv_plugin.py
+++ grr/lib/output_plugins/csv_plugin.py
@@ -10,98 +10,8 @@ import zipfile
 
 import yaml
 
-from grr.lib import export
 from grr.lib import instant_output_plugin
-from grr.lib import output_plugin
-from grr.lib import rdfvalue
 from grr.lib import utils
-from grr.lib.rdfvalues import structs as rdf_structs
-from grr.proto import output_plugin_pb2
-
-
-class CSVOutputPluginArgs(rdf_structs.RDFProtoStruct):
-  protobuf = output_plugin_pb2.CSVOutputPluginArgs
-
-
-# TODO(user): this is a legacy code, remove as soon as support
-# for legacy output plugins is removed. CSVInstantOutputPlugin is
-# supposed to be used instead.
-class CSVOutputPlugin(output_plugin.OutputPluginWithOutputStreams):
-  """Output plugin that writes hunt's results to a CSV file."""
-
-  name = "csv"
-  description = "Output ZIP archive with CSV files."
-  args_type = CSVOutputPluginArgs
-
-  def ProcessResponses(self, responses):
-    default_metadata = export.ExportedMetadata(
-        annotations=u",".join(self.args.export_options.annotations),
-        source_urn=self.state.source_urn)
-
-    if self.args.convert_values:
-      # This is thread-safe - we just convert the values.
-      converted_responses = export.ConvertValues(
-          default_metadata,
-          responses,
-          token=self.token,
-          options=self.args.export_options)
-    else:
-      converted_responses = responses
-
-    # This is not thread-safe, therefore WriteValueToCSVFile is synchronized.
-    self.WriteValuesToCSVFile(converted_responses)
-
-  def GetCSVHeader(self, value_class, prefix=""):
-    header = []
-    for type_info in value_class.type_infos:
-      if type_info.__class__.__name__ == "ProtoEmbedded":
-        header.extend(
-            self.GetCSVHeader(
-                type_info.type, prefix=prefix + type_info.name + "."))
-      else:
-        header.append(utils.SmartStr(prefix + type_info.name))
-
-    return header
-
-  def WriteCSVHeader(self, output_file, value_type):
-    value_class = rdfvalue.RDFValue.classes[value_type]
-    csv.writer(output_file).writerow(self.GetCSVHeader(value_class))
-
-  def GetCSVRow(self, value):
-    row = []
-    for type_info in value.__class__.type_infos:
-      if type_info.__class__.__name__ == "ProtoEmbedded":
-        row.extend(self.GetCSVRow(value.Get(type_info.name)))
-      else:
-        row.append(utils.SmartStr(value.Get(type_info.name)))
-
-    return row
-
-  def WriteCSVRow(self, output_file, value):
-    csv.writer(output_file).writerow(self.GetCSVRow(value))
-
-  def GetOutputFd(self, value_type):
-    """Initializes output AFF4Image for a given value type."""
-    file_name = value_type + ".csv"
-
-    output_stream = self._CreateOutputStream(file_name)
-
-    if output_stream.tell() == 0:
-      self.WriteCSVHeader(output_stream, value_type)
-
-    return output_stream
-
-  @utils.Synchronized
-  def WriteValuesToCSVFile(self, values):
-    output_files = {}
-    for value in values:
-      output_file = self.GetOutputFd(value.__class__.__name__)
-      output_files[value.__class__.__name__] = output_file
-
-      self.WriteCSVRow(output_file, value)
-
-    for fd in output_files.values():
-      fd.Flush()
 
 
 class CSVInstantOutputPlugin(
@@ -154,9 +64,9 @@ class CSVInstantOutputPlugin(
     if not first_value:
       return
 
-    yield self.archive_generator.WriteFileHeader("%s/%s/from_%s.csv" % (
-        self.path_prefix, first_value.__class__.__name__,
-        original_value_type.__name__))
+    yield self.archive_generator.WriteFileHeader(
+        "%s/%s/from_%s.csv" % (self.path_prefix, first_value.__class__.__name__,
+                               original_value_type.__name__))
 
     buf = cStringIO.StringIO()
     writer = csv.writer(buf)
--- grr/lib/output_plugins/csv_plugin_test.py
+++ grr/lib/output_plugins/csv_plugin_test.py
@@ -4,166 +4,16 @@
 
 import csv
 import os
-import StringIO
 import zipfile
 
 import yaml
 
 from grr.lib import flags
-from grr.lib import rdfvalue
 from grr.lib import test_lib
 from grr.lib.output_plugins import csv_plugin
 from grr.lib.output_plugins import test_plugins
 from grr.lib.rdfvalues import client as rdf_client
-from grr.lib.rdfvalues import flows as rdf_flows
 from grr.lib.rdfvalues import paths as rdf_paths
-
-
-class CSVOutputPluginTest(test_lib.FlowTestsBaseclass):
-  """Tests CSV hunt output plugin."""
-
-  def setUp(self):
-    super(CSVOutputPluginTest, self).setUp()
-    self.client_id = self.SetupClients(1)[0]
-    self.results_urn = self.client_id.Add("Results")
-    self.base_urn = rdfvalue.RDFURN("aff4:/foo/bar")
-
-  def ProcessResponses(self,
-                       plugin_args=None,
-                       responses=None,
-                       process_responses_separately=False):
-    plugin = csv_plugin.CSVOutputPlugin(
-        source_urn=self.results_urn,
-        output_base_urn=self.base_urn,
-        args=plugin_args,
-        token=self.token)
-    plugin.InitializeState()
-
-    messages = []
-    for response in responses:
-      messages.append(
-          rdf_flows.GrrMessage(
-              source=self.client_id, payload=response))
-
-    if process_responses_separately:
-      for message in messages:
-        plugin.ProcessResponses([message])
-    else:
-      plugin.ProcessResponses(messages)
-
-    plugin.Flush()
-
-    return plugin.OpenOutputStreams()
-
-  def testCSVPluginWithValuesOfSameType(self):
-    responses = []
-    for i in range(10):
-      responses.append(
-          rdf_client.StatEntry(
-              pathspec=rdf_paths.PathSpec(
-                  path="/foo/bar/%d" % i, pathtype="OS"),
-              st_mode=33184,  # octal = 100640 => u=rw,g=r,o= => -rw-r-----
-              st_ino=1063090,
-              st_dev=64512L,
-              st_nlink=1 + i,
-              st_uid=139592,
-              st_gid=5000,
-              st_size=0,
-              st_atime=1336469177,
-              st_mtime=1336129892,
-              st_ctime=1336129892))
-
-    streams = self.ProcessResponses(
-        plugin_args=csv_plugin.CSVOutputPluginArgs(), responses=responses)
-    self.assertEqual(streams.keys(), ["ExportedFile.csv"])
-    self.assertEqual(streams["ExportedFile.csv"].urn,
-                     rdfvalue.RDFURN("aff4:/foo/bar/ExportedFile.csv"))
-
-    contents = StringIO.StringIO(streams["ExportedFile.csv"].Read(16384))
-    parsed_output = list(csv.DictReader(contents))
-    self.assertEqual(len(parsed_output), 10)
-    for i in range(10):
-      self.assertEqual(parsed_output[i]["metadata.client_urn"], self.client_id)
-      self.assertEqual(parsed_output[i]["metadata.hostname"], "Host-0")
-      self.assertEqual(parsed_output[i]["metadata.mac_address"],
-                       "aabbccddee00\nbbccddeeff00")
-      self.assertEqual(parsed_output[i]["metadata.source_urn"],
-                       self.results_urn)
-      self.assertEqual(parsed_output[i]["metadata.hardware_info.bios_version"],
-                       "Bios-Version-0")
-
-      self.assertEqual(parsed_output[i]["urn"],
-                       self.client_id.Add("/fs/os/foo/bar").Add(str(i)))
-      self.assertEqual(parsed_output[i]["st_mode"], "-rw-r-----")
-      self.assertEqual(parsed_output[i]["st_ino"], "1063090")
-      self.assertEqual(parsed_output[i]["st_dev"], "64512")
-      self.assertEqual(parsed_output[i]["st_nlink"], str(1 + i))
-      self.assertEqual(parsed_output[i]["st_uid"], "139592")
-      self.assertEqual(parsed_output[i]["st_gid"], "5000")
-      self.assertEqual(parsed_output[i]["st_size"], "0")
-      self.assertEqual(parsed_output[i]["st_atime"], "2012-05-08 09:26:17")
-      self.assertEqual(parsed_output[i]["st_mtime"], "2012-05-04 11:11:32")
-      self.assertEqual(parsed_output[i]["st_ctime"], "2012-05-04 11:11:32")
-      self.assertEqual(parsed_output[i]["st_blksize"], "0")
-      self.assertEqual(parsed_output[i]["st_rdev"], "0")
-      self.assertEqual(parsed_output[i]["symlink"], "")
-
-  def testCSVPluginWithValuesOfMultipleTypes(self):
-    streams = self.ProcessResponses(
-        plugin_args=csv_plugin.CSVOutputPluginArgs(),
-        responses=[
-            rdf_client.StatEntry(pathspec=rdf_paths.PathSpec(
-                path="/foo/bar", pathtype="OS")), rdf_client.Process(pid=42)
-        ],
-        process_responses_separately=True)
-
-    self.assertEqual(
-        sorted(streams.keys()),
-        sorted(["ExportedFile.csv", "ExportedProcess.csv"]))
-    self.assertEqual(streams["ExportedFile.csv"].urn,
-                     rdfvalue.RDFURN("aff4:/foo/bar/ExportedFile.csv"))
-    self.assertEqual(streams["ExportedProcess.csv"].urn,
-                     rdfvalue.RDFURN("aff4:/foo/bar/ExportedProcess.csv"))
-
-    contents = StringIO.StringIO(streams["ExportedFile.csv"].Read(16384))
-    parsed_output = list(csv.DictReader(contents))
-
-    self.assertEqual(len(parsed_output), 1)
-
-    self.assertEqual(parsed_output[0]["metadata.client_urn"], self.client_id)
-    self.assertEqual(parsed_output[0]["metadata.hostname"], "Host-0")
-    self.assertEqual(parsed_output[0]["metadata.mac_address"],
-                     "aabbccddee00\nbbccddeeff00")
-    self.assertEqual(parsed_output[0]["metadata.source_urn"], self.results_urn)
-    self.assertEqual(parsed_output[0]["urn"],
-                     self.client_id.Add("/fs/os/foo/bar"))
-
-    contents = StringIO.StringIO(streams["ExportedProcess.csv"].Read(16384))
-    parsed_output = list(csv.DictReader(contents))
-    self.assertEqual(len(parsed_output), 1)
-
-    self.assertEqual(parsed_output[0]["metadata.client_urn"], self.client_id)
-    self.assertEqual(parsed_output[0]["metadata.hostname"], "Host-0")
-    self.assertEqual(parsed_output[0]["metadata.mac_address"],
-                     "aabbccddee00\nbbccddeeff00")
-    self.assertEqual(parsed_output[0]["metadata.source_urn"], self.results_urn)
-    self.assertEqual(parsed_output[0]["pid"], "42")
-
-  def testCSVPluginWritesUnicodeValuesCorrectly(self):
-    streams = self.ProcessResponses(
-        plugin_args=csv_plugin.CSVOutputPluginArgs(),
-        responses=[
-            rdf_client.StatEntry(pathspec=rdf_paths.PathSpec(
-                path="/中国新闻网新闻中", pathtype="OS"))
-        ],
-        process_responses_separately=True)
-
-    contents = StringIO.StringIO(streams["ExportedFile.csv"].Read(16384))
-    parsed_output = list(csv.DictReader(contents))
-
-    self.assertEqual(len(parsed_output), 1)
-    self.assertEqual(parsed_output[0]["urn"],
-                     self.client_id.Add("/fs/os/中国新闻网新闻中"))
 
 
 class CSVInstantOutputPluginTest(test_plugins.InstantOutputPluginTestBase):
