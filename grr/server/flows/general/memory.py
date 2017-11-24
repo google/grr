c1f98adca273448f10f4f732b2aed22fd469dd48
Andreas Moser <amoser@google.com>
2017-11-24 11:58:23 +0100

    Fixing a breakage introduced by Rekall in the MemoryCollector.

--- grr/server/flows/general/memory.py
+++ grr/server/flows/general/memory.py
@@ -87,7 +87,7 @@ class MemoryCollector(flow.GRRFlow):
 
   def RunRekallPlugin(self):
     plugin = rekall_types.PluginRequest(plugin="aff4acquire")
-    plugin.args["destination"] = "file:GRR"
+    plugin.args["destination"] = "GRR"
     request = rekall_types.RekallRequest(plugins=[plugin])
 
     # Note that this will actually also retrieve the memory image.
