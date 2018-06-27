3513f59c7c916962e7f22ea4073534bc81a1a9eb
Andreas Moser <amoser@google.com>
2018-06-27 11:29:51 +0200

    Grabbing the syslog on Appveyor e2e test runs.

--- appveyor/linux/upload_e2e_artifacts.sh
+++ appveyor/linux/upload_e2e_artifacts.sh
@@ -10,16 +10,17 @@ readonly INITIAL_DIR="${PWD}"
 mkdir appveyor_e2e_artifacts
 cd appveyor_e2e_artifacts
 
-mkdir server-configs server-logs client-configs client-logs
+mkdir server-configs server-logs client-configs client-logs machine-logs
 sudo cp /usr/share/grr-server/install_data/etc/grr-server.yaml server-configs/ || true # Primary server config.
 sudo cp /etc/grr/server.local.yaml server-configs/ || true # Server writeback.
 sudo cp /usr/share/grr-server/lib/python2.7/site-packages/grr/var/log/* server-logs/ || true
 sudo cp /usr/lib/grr/grr_*_amd64/grrd.yaml client-configs/ || true # Primary client config.
 sudo cp /etc/grr.local.yaml client-configs/ || true # Secondary client config.
 sudo cp /var/log/GRRlog.txt client-logs/ || true
+sudo cp /var/log/syslog* machine-logs/ || true
 
 # Give read permissions to the non-root user.
-sudo chown -R "$(whoami):$(whoami)" server-configs server-logs client-configs client-logs
+sudo chown -R "$(whoami):$(whoami)" server-configs server-logs client-configs client-logs machine-logs
 
 cd "${INITIAL_DIR}"
 
@@ -40,4 +41,8 @@ done
 
 for log in appveyor_e2e_artifacts/client-logs/*; do
   appveyor PushArtifact "${log}" -DeploymentName 'Client Logs'
+done
+
+for log in appveyor_e2e_artifacts/machine-logs/*; do
+  appveyor PushArtifact "${log}" -DeploymentName 'Machine Logs'
 done
