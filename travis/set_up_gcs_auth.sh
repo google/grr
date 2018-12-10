179f702feb9964af3578a23e7dd6be288ecffbd3
Łukasz Hanuszczak <mrhania@users.noreply.github.com>
2018-12-10 14:26:03 +0100

    Try to fix Travis issues once again.

--- travis/set_up_gcs_auth.sh
+++ travis/set_up_gcs_auth.sh
@@ -1,14 +1,8 @@
 set -e
 
-if [[ "$(which gcloud)" ]]; then
-  echo "Google Cloud SDK already installed"
-  gcloud version
-else
-  echo "Google Cloud SDK not found. Downloading.."
-  # The linux sdk seems to work on Mac..¯\_(ツ)_/¯
-  wget -q https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-116.0.0-linux-x86_64.tar.gz
-  tar zxf google-cloud-sdk-116.0.0-linux-x86_64.tar.gz -C "${HOME}"
-fi
+# The linux sdk seems to work on Mac..¯\_(ツ)_/¯
+wget -q https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-116.0.0-linux-x86_64.tar.gz
+tar zxf google-cloud-sdk-116.0.0-linux-x86_64.tar.gz -C "${HOME}"
 
 # See https://docs.travis-ci.com/user/encrypting-files/
 openssl aes-256-cbc -K "$encrypted_db009a5a71c6_key" \
