517e95bf6e23e40ba84d4f6b247f6e3ad4041529
Andy Gu <andygu@spotify.com>
2019-01-22 18:27:01 +0000

    Remove License from validate_iap.py

--- grr/server/grr_response_server/gui/validate_iap.py
+++ grr/server/grr_response_server/gui/validate_iap.py
@@ -1,17 +1,3 @@
-# Copyright 2016 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Sample showing how to validate the Identity-Aware Proxy (IAP) JWT.
 
 This code should be used by applications in Google Compute Engine-based
@@ -22,7 +8,6 @@ of assurance that a request was authorized by IAP.
 For applications running in the App Engine standard environment, use
 App Engine's Users API instead.
 """
-# [START iap_validate_jwt]
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
@@ -94,5 +79,3 @@ def GetIapKey(key_id):
         if not key:
             raise Exception('Key {!r} not found'.format(key_id))
     return key
-
-# [END iap_validate_jwt]
