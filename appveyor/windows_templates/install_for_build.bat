dee93cc1802e14fb2794645196f1d0fa37b0d29b
Mikhail Bushkov <mbushkov@google.com>
2019-11-18 23:39:47 +0100

    Removing traces of Python27 from AppVeyor jobs.

--- appveyor/windows_templates/install_for_build.bat
+++ appveyor/windows_templates/install_for_build.bat
@@ -12,7 +12,7 @@ C:\Python36\python.exe -m pip install --upgrade
 :: Install Google Cloud SDK
 echo Installing Google Cloud SDK
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "(new-object System.Net.WebClient).DownloadFile('https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-163.0.0-windows-x86_64.zip', 'C:\grr_deps\google-cloud-sdk-163.0.0-windows-x86_64.zip')"
-C:\Python27-x64\python.exe -m "zipfile" -e C:\grr_deps\google-cloud-sdk-163.0.0-windows-x86_64.zip C:\grr_deps
+C:\Python36-x64\python.exe -m "zipfile" -e C:\grr_deps\google-cloud-sdk-163.0.0-windows-x86_64.zip C:\grr_deps
 C:\grr_deps\google-cloud-sdk\install.bat --quiet || echo "Google Cloud SDK installation failed" && exit /b 1
 
 echo GRR dependency installation complete
