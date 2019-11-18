34347b541db2665d5573349bc537e372f93d96e9
Mikhail Bushkov <mbushkov@google.com>
2019-11-19 00:02:18 +0100

    Commenting out cache.

--- appveyor/windows_templates/install_for_build.bat
+++ appveyor/windows_templates/install_for_build.bat
@@ -4,10 +4,7 @@ mkdir C:\grr_deps || echo "Failed to create C:\grr_deps" && exit /b 1
 cd C:\grr_deps
 
 C:\Python36-x64\python.exe --version || echo "64bit python missing" && exit /b 1
-C:\Python36-x64\python.exe -m pip install --upgrade
-
-C:\Python36\python.exe --version || echo "32bit python install failed" && exit /b 1
-C:\Python36\python.exe -m pip install --upgrade
+C:\Python36\python.exe --version || echo "32bit python install failed" && exit /
 
 :: Install Google Cloud SDK
 echo Installing Google Cloud SDK
