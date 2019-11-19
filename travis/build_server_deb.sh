d9cdd6e204c3e8ffd60166bea0fb30e886476ceb
Mikhail Bushkov <mbushkov@google.com>
2019-11-19 17:46:11 +0100

    Adapting to Python 3.

--- travis/build_server_deb.sh
+++ travis/build_server_deb.sh
@@ -17,7 +17,7 @@ print('%s.%s.%s-%s' % (
     config.get('Version', 'revision'),
     config.get('Version', 'release')))
 "
-  deb_version="$(/usr/local/bin/python3.6 -c "${pyscript}")"
+  deb_version="$(python3 -c "${pyscript}")"
   debchange --create \
       --newversion "${deb_version}" \
       --package grr-server \
