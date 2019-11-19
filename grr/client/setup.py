e05de2403cb37992ff207c1a126b83423226fb5d
Mikhail Bushkov <mbushkov@google.com>
2019-11-19 13:09:52 +0100

    Fix extras_require syntax.

--- grr/client/setup.py
+++ grr/client/setup.py
@@ -94,7 +94,7 @@ setup_args = dict(
 if platform.system() == "Linux":
   # TODO(mbushkov): change 'extras_require' to 'install_requires' if/when
   # chipsec driver-less PIP package shows up on PyPI.
-  setup_args["extras_require"].append("chipsec==1.4.3")
+  setup_args["extras_require"]["chipsec"] = ["chipsec==1.4.3"]
 
 if platform.system() != "Windows":
   setup_args["install_requires"].append("xattr==0.9.6")
