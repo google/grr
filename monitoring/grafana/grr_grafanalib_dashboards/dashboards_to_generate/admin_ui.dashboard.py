8621aa0d0c69c7b1a06b3fb7c336a314d11aa059
Gilad Tsehori <giladt@google.com>
2020-06-15 13:46:42 +0000

    Added new dashboard for AdminUI component

---
+++ monitoring/grafana/grr_grafanalib_dashboards/dashboards_to_generate/admin_ui.dashboard.py
@@ -0,0 +1,39 @@
+from grafanalib.core import (
+    Dashboard, Graph, Row, Target
+)
+from grr_grafanalib_dashboards.util import add_data_source
+from grr_grafanalib_dashboards.config import PANELS
+
+GRR_COMPONENT = "admin_ui"
+
+dashboard = Dashboard(
+    title="{}s Dashboard".format(GRR_COMPONENT).title(),
+    rows=[
+        Row(panels=[
+            panel(GRR_COMPONENT) for panel in PANELS
+        ]
+        ),
+        Row(panels=[
+          Graph(
+            title="API Method Latency Rate",
+            targets=[
+                Target(
+                    expr='sum(rate(api_method_latency_sum[5m])) / sum(rate(api_method_latency_count[5m]))',
+                    legendFormat="Latency",
+                ),
+            ],
+            ),
+            Graph(
+            title="API Access Probe Latency",
+            targets=[
+                Target(
+                    expr='sum(rate(api_access_probe_latency_sum[5m])) / sum(rate(api_access_probe_latency_count[5m]))',
+                    legendFormat="Latency",
+                ),
+            ],
+            ),
+        ]),
+    ],
+).auto_panel_ids()
+
+dashboard = add_data_source(dashboard)
