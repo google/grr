01b233cc38b389127173abf0a640f3687ff2c14d
Gilad Tsehori <giladt@google.com>
2020-06-15 10:04:52 +0000

    Removed comments and TODOs
    Version is unnecessary for grr-grafanalib-dashboards, as it is a utility package, which will not be a part of the GRR server deb package.

--- monitoring/grafana/setup.py
+++ monitoring/grafana/setup.py
@@ -1,9 +1,7 @@
 import setuptools
 
-# TODO figure out version, cmdclas, data
 setup_args = dict(
     name="grr-grafanalib-dashboards",
-    # version=VERSION.get("Version", "packageversion"),
     description="GRR grafanalib Monitoring Dashboards",
     license="Apache License, Version 2.0",
     url="https://github.com/google/grr/tree/master/monitoring/grafana",
@@ -16,10 +14,6 @@ setup_args = dict(
         # we'll try hard not to", so we stick with version 0.5.7.
         "grafanalib==0.5.7",
     ],
-    # cmdclass={
-    #     "sdist": Sdist,
-    # },
-    # data=["version.ini"]
     )
 
 setuptools.setup(**setup_args)
