404e1f1e73d1fc6a242c88afa7cefbb2efda799a
Gilad Tsehori <giladt@google.com>
2020-06-15 10:19:40 +0000

    Deleted generate_dashboards script

--- monitoring/grafana/grr_grafanalib_dashboards/generate_dashboards.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# generate-dashboard -o grafanalib_dashboards/frontends_dashboard.json grafanalib_dashboards/frontends_dashboard.py
-# generate-dashboard -o grafanalib_dashboards/workers_dashboard.json grafanalib_dashboards/workers_dashboard.py
-#export PYTHONPATH="/usr/local/google/home/giladt/Desktop/grafanalib_dashboards:$PYTHONPATH"
-for dashboard in grafanalib_dashboards/*_dashboard.py;
-do
-    generate-dashboard -o ${dashboard%.*}.json $dashboard
-done
