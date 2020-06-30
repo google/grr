944a46ac158c0ea966ac3059827a88525e1e78fe
Gilad Tsehori <giladt@google.com>
2020-06-30 15:04:18 +0000

    Created Fleetspeak dashboard with some initial panels

---
+++ monitoring/grafana/grr_grafanalib_dashboards/dashboards_to_generate/fleetspeak.dashboard.py
@@ -0,0 +1,81 @@
+from grafanalib.core import Alert, AlertCondition, Dashboard, Graph, LowerThan, OP_AND, Row, RTYPE_SUM, Target, TimeRange, YAxes, YAxis
+from grr_grafanalib_dashboards.util import add_data_source
+from grr_grafanalib_dashboards.config import ACTIVE_PROCESSES_ALERTING_CONDITION
+
+dashboard = Dashboard(
+  title="Fleetspeak Servers Dashboard",
+  rows=[
+    Row(panels=[
+      Graph(
+        title="Number of Active Processes",
+        targets=[
+          Target(
+            expr='sum(up{job="fleetspeak"})',
+            legendFormat="Active Processes",
+          ),
+        ],
+        alert=Alert(
+          name="Number of Active Processes alert",
+          message="The number of active Fleetspeak Server processes is below {}".format(ACTIVE_PROCESSES_ALERTING_CONDITION),
+          alertConditions=[
+            AlertCondition(
+              Target(
+                expr='sum(up{job="fleetspeak"})',
+                legendFormat="Active Processes",
+              ),
+              timeRange=TimeRange("10s", "now"),
+              evaluator=LowerThan(ACTIVE_PROCESSES_ALERTING_CONDITION),
+              operator=OP_AND,
+              reducerType=RTYPE_SUM
+            )
+          ],
+        )
+      ),
+      Graph(
+        title="Sum of Process Memory Bytes (across all instances)",
+        targets=[
+          Target(
+            expr='sum(process_resident_memory_bytes{job="fleetspeak"})',
+            legendFormat="Resident Memory",
+          ),
+        ]
+      ),
+      Graph(
+        title="CPU Usage",
+        targets=[
+          Target(
+            expr='avg(rate(process_cpu_seconds_total{job="fleetspeak"}[30s])) * 100',
+            legendFormat="Average Process CPU Usage in %",
+          ),
+        ],
+        yAxes=YAxes(
+          left=YAxis(max=105)
+        ),
+      ),
+    ]
+    ),
+    Row(panels=[
+      Graph(
+        title="Database Operations Latency",
+        targets=[
+          Target(
+            expr='rate(fleetspeak_server_datastore_operations_completed_sum[10m]) / rate(fleetspeak_server_datastore_operations_completed_count[10m])',
+            legendFormat="Latency - Operation: {{operation}}",
+          ),
+        ]
+      ),
+      Graph(
+        title="Database Operations Errors Rate",
+        targets=[
+          Target(
+            expr='rate(fleetspeak_server_datastore_operations_completed_count{errored="true"}[10m])',
+            legendFormat="Operations Error Rate - Operation: {{operation}}",
+          ),
+        ]
+      ),
+      ]
+    ),
+  ]
+).auto_panel_ids()
+
+dashboard = add_data_source(dashboard, "fleetspeak")
