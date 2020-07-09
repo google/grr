7f560ca001aa8a74720f684abf990fb46249d959
Daniel Berbece <danielberbece@google.com>
2020-07-09 10:16:12 +0000

    Add copy button for complete timestamp

--- grr/server/grr_response_server/gui/ui/components/timestamp/timestamp.ts
+++ grr/server/grr_response_server/gui/ui/components/timestamp/timestamp.ts
@@ -12,5 +12,5 @@ export class Timestamp {
   @Input() date?: Date;
   @Input() completeFormat: boolean = false;
   @Input() tooltipEnabled: boolean = true;
-  timezone: string = 'UTC'
+  readonly timezone: string = 'UTC';
 }
