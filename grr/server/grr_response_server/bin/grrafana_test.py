37978382dff4a14983fb3fd7fbf993c786c664b8
Gilad Tsehori <giladt@google.com>
2020-08-26 12:15:25 +0000

    Added structure code for Grrafana object tests

---
+++ grr/server/grr_response_server/bin/grrafana_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Lint as: python3
+"""Unittest for GRRafana HTTP server."""
+from grr.test_lib import test_lib
+from grr_response_server.bin import grrafana
+
+# TODO(tsehori): We should implement tests for grrafana.py.
+class GRRafanaTest(test_lib.GRRBaseTest):
+  """Test the GRRafana HTTP server."""
+
+  pass
