2074a189e52f24c06bac9d5cc91dc79bf6497b4a
Gilad Tsehori <giladt@google.com>
2020-08-30 13:28:06 +0000

    Removed unnecessary imports

--- grr/server/grr_response_server/bin/grrafana_test.py
+++ grr/server/grr_response_server/bin/grrafana_test.py
@@ -2,7 +2,7 @@
 # Lint as: python3
 """Unittest for GRRafana HTTP server."""
 from absl.testing import absltest
-from absl import app, flags
+from absl import app
 import mock
 
 from fleetspeak.src.server.proto.fleetspeak_server import admin_pb2, resource_pb2
@@ -10,7 +10,6 @@ from fleetspeak.src.server.proto.fleetspeak_server import admin_pb2, resource_pb
 from grr_response_server import fleetspeak_connector
 from grr_response_server import fleetspeak_utils
 from grr_response_server.bin import grrafana
-from grr_response_server import server_startup
 
 from werkzeug import serving as werkzeug_serving
 from werkzeug import test as werkzeug_test
