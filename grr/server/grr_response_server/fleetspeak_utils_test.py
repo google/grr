25044f2446766b73f43dcffaa406f442823bfe1f
Gilad Tsehori <giladt@google.com>
2020-08-30 07:52:13 +0000

    Removed leftover references to _stub

--- grr/server/grr_response_server/fleetspeak_utils_test.py
+++ grr/server/grr_response_server/fleetspeak_utils_test.py
@@ -145,7 +145,7 @@ class FleetspeakUtilsTest(test_lib.GRRBaseTest):
 
   def testFetchClientResourceUsageRecordsFromFleetspeak(self):
     conn = mock.MagicMock()
-    conn.outgoing._stub.FetchClientResourceUsageRecords.return_value = admin_pb2.FetchClientResourceUsageRecordsResponse(
+    conn.outgoing.FetchClientResourceUsageRecords.return_value = admin_pb2.FetchClientResourceUsageRecordsResponse(
         records=[{
             "mean_user_cpu_rate": 1,
             "max_system_cpu_rate": 2
@@ -163,8 +163,8 @@ class FleetspeakUtilsTest(test_lib.GRRBaseTest):
       self.assertListEqual(
           fleetspeak_utils.FetchClientResourceUsageRecordsFromFleetspeak(
               _TEST_CLIENT_ID, 10), expected_records_list)
-      conn.outgoing._stub.FetchClientResourceUsageRecords.assert_called_once()
-      insert_args, _ = conn.outgoing._stub.FetchClientResourceUsageRecords.call_args
+      conn.outgoing.FetchClientResourceUsageRecords.assert_called_once()
+      insert_args, _ = conn.outgoing.FetchClientResourceUsageRecords.call_args
       fs_message = insert_args[0]
       self.assertEqual(
           fs_message,
