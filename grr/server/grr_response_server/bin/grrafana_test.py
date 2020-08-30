b850123b9752dbe933efd50aea109be989f055df
Gilad Tsehori <giladt@google.com>
2020-08-30 09:16:23 +0000

    Fixed relevant imports for Grrafana testing

--- grr/server/grr_response_server/bin/grrafana_test.py
+++ grr/server/grr_response_server/bin/grrafana_test.py
@@ -2,8 +2,15 @@
 # Lint as: python3
 """Unittest for GRRafana HTTP server."""
 from absl.testing import absltest
+from absl import app
 
+from grr_response_server import fleetspeak_connector
 from grr_response_server.bin import grrafana
+from grr_response_server import server_startup
+
+from werkzeug import serving as werkzeug_serving
+from werkzeug import test as werkzeug_test
+
 
 # TODO(tsehori): We should implement tests for grrafana.py.
 class GrrafanaTest(absltest.TestCase):
