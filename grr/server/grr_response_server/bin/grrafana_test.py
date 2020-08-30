b20aee5cf9f9fd474df14fad088584c52a70c7bd
Gilad Tsehori <giladt@google.com>
2020-08-30 09:19:59 +0000

    Added initial testing for GRRafana HTTP server
    This commit includes two basic tests for '/' and '/search' endpoints
    (only partially for '/search') and also a structuring of the tests wth
    main function.

--- grr/server/grr_response_server/bin/grrafana_test.py
+++ grr/server/grr_response_server/bin/grrafana_test.py
@@ -12,8 +12,31 @@ from werkzeug import serving as werkzeug_serving
 from werkzeug import test as werkzeug_test
 
 
-# TODO(tsehori): We should implement tests for grrafana.py.
 class GrrafanaTest(absltest.TestCase):
   """Test the GRRafana HTTP server."""
 
-  pass
+  def testRoot(self):
+    client = werkzeug_test.Client(grrafana.Grrafana(), grrafana.JSONResponse)
+    response = client.get('/')
+    self.assertEqual(200, response.status_code)
+
+  def testSearchMetrics(self):
+    client = werkzeug_test.Client(application=grrafana.Grrafana(),
+                                  response_wrapper=grrafana.JSONResponse)
+    response = client.post("/search", json={'type': 'timeseries', 'target': ''})
+    self.assertEqual(response.json, [
+        "mean_user_cpu_rate",
+        "max_user_cpu_rate",
+        "mean_system_cpu_rate",
+        "max_system_cpu_rate",
+        "mean_resident_memory_mib",
+        "max_resident_memory_mib",
+    ])
+
+
+def main(argv):
+  absltest.main(argv)
+
+
+if __name__ == "__main__":
+  app.run(main)
