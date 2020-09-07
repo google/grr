60d36ea2bf8c2dced9e4155d5c762574b8f63df6
Daniel Berbece <danielberbece@google.com>
2020-09-07 14:57:53 +0000

    Reenable closing

--- grr/server/grr_response_server/gui/ui/components/client/client.ts
+++ grr/server/grr_response_server/gui/ui/components/client/client.ts
@@ -2,10 +2,9 @@ import {ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewChild} from '
 import {MatDrawer} from '@angular/material/sidenav';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Subject} from 'rxjs';
-import {filter, map, take, takeUntil} from 'rxjs/operators';
+import {filter, map, takeUntil} from 'rxjs/operators';
 
 import {ClientPageFacade} from '../../store/client_page_facade';
-import {ClientOverview} from '../client_overview/client_overview';
 
 /**
  * Component displaying the details and actions for a single Client.
