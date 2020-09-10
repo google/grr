0848955f6f4f09008e0f91707d5d92cc119b8c82
Daniel Berbece <danielberbece@google.com>
2020-09-10 16:29:34 +0000

    Entry history components (part 2) (#842)
    * Add details page base module
    
    * Add client details module to app module
    
    * Add Agent details section
    
    * Add os info section
    
    * Add RDF value descriptors to ConfigFacade
    
    * Add user proto mapping
    
    * Add info to client and update tests
    
    * Add jsDoc and remove some comments
    
    * Add component for showing size in a human readable format
    
    * Add cpu and memory info
    
    * Add users list
    
    * Add ApiVolume
    
    * Add volumes basic info
    
    * Add labels
    
    * Reorder sections
    
    * Display general info
    
    * Update tests and rollback changes on human readable size
    
    * Improve format and prevent undefined errors
    
    * Add OS specific volume details
    
    * Add some style
    
    * Style details button
    
    * Use newClient() for all client object creation in tests
    
    * Use RouterLink for opening client details page
    
    * Use last variable from ngFor
    
    * Remove reflection data fetching and storing logic
    
    * Support uint64 values
    
    * Replace || with ??
    
    * Update primitive translation tests
    
    * Add additional tests for human readable size component
    
    * Remove unused router
    
    * Check for undefined values
    
    * Throw original error for decodeBase64()
    
    * Add comment to explain why math was needed
    
    * Update test
    
    * Enforce difference between values and labels
    
    * Enforce difference between values and labels for client labels too
    
    * Enforce FQDN
    
    * Add fetching of client versions to ClientPageFacade
    
    * Add age property to client model
    
    * Add tests for selectedClientVersions
    
    * Add timeline div in html
    
    * Remove debug logs
    
    * Add deep-diff package
    
    * Use classic array instead of readonly to provide array processing
    
    * Add client versions to timeline
    
    * Style timeline entry
    
    * Make age mandatory in client model
    
    * Test getClientVersions
    
    * Style timeline
    
    * Add current client-only properties
    
    * Add accordion to lists
    
    * Add tests
    
    * Remove deployment file
    
    * Add timeline entry description algorithm
    
    * Correct plural use for accordion button
    
    * Add suggested changes
    
    * Remove ApiGetClientVersionsArgs
    
    * Cleanup typografy styling
    
    * Add removal of client label
    
    * Use consistent naming
    
    * Add snackbar for client label removal
    
    * Add labels trackByName
    
    * Address review suggestions
    
    * Add new tests
    
    * Simplify store implementation of label removal tracking
    
    * Show remove client label error
    
    * Remove show remove label error
    
    * Add diff functionality for changed entries
    
    * Add basic drawer implementation
    
    * Disable page scrolling on drawer open and remove unneeded info from client details
    
    * Change button style
    
    * Change url on client details opening
    
    * Test drawer with client details
    
    * Refactor the client overview into a new component
    
    * Add dialog basic implementation
    
    * Add entry history button component
    
    * Add entry type input
    
    * Add suggested changes
    
    * Use css variables
    
    * Delete client details header and add ngIf guard
    
    * Style button
    
    * Add pluralization and default snapshot
    
    * Remove unused EntryChanges interface
    
    * Test EntryHistoryDialog
    
    * Refactor URL routing logic
    
    * Create user_details, volume_details and interface_details
    
    * Show array details and update entry history dialog
    
    * Update diffing with changes of the sub-properties
    
    * Add tests
    
    * Add suggested changes
    
    * Remove comment
    
    * Refactor entry history dialog tests
    
    * Apply suggested changes
    
    * Add module file for sub-components
    
    * Add ignore tslint

---
+++ grr/server/grr_response_server/gui/ui/components/client_details/interfaces_details/interfaces_details.ts
@@ -0,0 +1,15 @@
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {NetworkInterface} from '@app/lib/models/client';
+
+/**
+ * Component the details for a single NetworkInterface.
+ */
+@Component({
+  selector: 'interfaces-details',
+  templateUrl: './interfaces_details.ng.html',
+  styleUrls: ['./interfaces_details.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InterfacesDetails {
+  @Input() interfaces!: NetworkInterface[];
+}
