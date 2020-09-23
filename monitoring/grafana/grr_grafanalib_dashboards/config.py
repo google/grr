a35f75e37a38797b07939bd40b3d3a381204025e
Gilad Tsehori <giladt@google.com>
2020-09-23 11:23:23 +0000

    Fixed config.py file

--- monitoring/grafana/grr_grafanalib_dashboards/config.py
+++ monitoring/grafana/grr_grafanalib_dashboards/config.py
@@ -1,12 +1,13 @@
 from grr_grafanalib_dashboards import reusable_panels
 
-# The data source name is specified after Grafana is set up
+# The data source names are specified after Grafana is set up
 # and it can be visited at localhost:3000.
-# In GRR Monitoring docs, we suggest naming it "grr-server", but if it's
-# not the case, change it here.
+# In GRR Monitoring docs, we suggest naming the data sources "grr-server"
+# and "grrafana" respectively, but if it's not the case, change it here.
 # For reference, take a look at the docs here:
 # https://grr-doc.readthedocs.io/en/latest/maintaining-and-tuning/monitoring.html#example-visualization-and-alerting-setup
 GRAFANA_DATA_SOURCE = "grr-server"
+CLIENT_LOAD_STATS_DATA_SOURCE = "grrafana"
 
 # An alert will be fired if the number of active processes (of any
 # GRR server component) is below this number.
