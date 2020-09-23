3f810d9b64507800497c6004da841313e05e365d
Alex Mihai <alexmihai@google.com>
2020-09-23 09:28:13 +0000

    Try 3 generation: Fix venv activate path and local_pypi path

--- travis/build_api_documentation.sh
+++ travis/build_api_documentation.sh
@@ -3,29 +3,28 @@
 # TODO: Delete debug commands below
 pwd
 ls -ltr .
-ls -ltr grr/local_pypi
-ls -ltr
-tree -L 2
+ls -ltr local_pypi
 # TODO: Delete above
 
 function setup_environment() {
   # Set up virtual python and node environment.
   python3 -m venv "${HOME}/DOCUMENTATION_VENV"
-  source "${HOME}/DOCUMENTATION_VENV/activate"
+  source "${HOME}/DOCUMENTATION_VENV/bin/activate"
+  pip3 install --upgrade pip wheel six setuptools nodeenv
   nodeenv -p --prebuilt --node=12.18.0
-  source "${HOME}/DOCUMENTATION_VENV/activate"
+  source "${HOME}/DOCUMENTATION_VENV/bin/activate"
 
   # Install GRR pip packages from local pyindex.
   pip install --no-index --no-cache-dir --find-links=grr/local_pypi \
-    grr/local_pypi/grr-response-proto-*.zip \
-    grr/local_pypi/grr-response-core-*.zip \
-    grr/local_pypi/grr-response-client-*.zip \
-    grr/local_pypi/grr-api-client-*.zip \
-    grr/local_pypi/grr-response-server-*.zip
+    local_pypi/grr-response-proto-*.zip \
+    local_pypi/grr-response-core-*.zip \
+    local_pypi/grr-response-client-*.zip \
+    local_pypi/grr-api-client-*.zip \
+    local_pypi/grr-response-server-*.zip
 }
 
 function generate_openapi_description() {
-  "${HOME}/DOCUMENTATION_VENV/bin/python" "travis/get_openapi_description.py"
+  python3 "travis/get_openapi_description.py"
 }
 
 function generate_documentation() {
@@ -37,3 +36,5 @@ function generate_documentation() {
 setup_environment
 generate_openapi_description
 generate_documentation
+
+deactivate
