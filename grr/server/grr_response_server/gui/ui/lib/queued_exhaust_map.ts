911e556322a90a035ddefcb10d87aa4da17594c6
Simeon Stoykov <simstoykov@google.com>
2020-10-27 11:51:42 +0000

    queued exhaust map functionality

--- grr/server/grr_response_server/gui/ui/lib/queued_exhaust_map.ts
+++ grr/server/grr_response_server/gui/ui/lib/queued_exhaust_map.ts
@@ -1,24 +1,43 @@
 import { ObservableInput, OperatorFunction, ObservedValueOf, pipe, EMPTY } from 'rxjs';
 import { concatMap, tap } from 'rxjs/operators';
 
-/** TODO */
+class NotImplementedError extends Error { };
+
+class RequestTracker<T> {
+  lastValue?: T;
+  isLastValueSet = false;
+
+  recordLast(value: T): void {
+    this.lastValue = value;
+    this.isLastValueSet = true;
+  }
+
+  shouldProcess(value: T): boolean {
+    return this.isLastValueSet && this.lastValue === value;
+  }
+}
+
+/**
+ * Similar to exhaustMap, but buffers the specified number of most recent source values
+ * in a queue of limited size. The source values that are not discarded are projected to
+ * Observables and merged in the output Observable in a serialized fashion, waiting for
+ * each one to complete before merging the next.
+ */
 export function queuedExhaustMap<T, O extends ObservableInput<any>> (
-  project: (value: T, index: number) => O
+  project: (value: T, index: number) => O, queueSize: 1 = 1
 ): OperatorFunction<T, ObservedValueOf<O>> {
-
-  function recordLastRequest(value: T): void {
-
+  if (queueSize !== 1) {
+    throw new NotImplementedError('Queue size different than 1 is not implemented yet.');
   }
-  function isLastRequest(value: T): boolean {
-    return true;
-  }
+
+  const requestTracker = new RequestTracker<T>();
 
   return pipe(
     tap((value) => {
-      recordLastRequest(value);
+      requestTracker.recordLast(value);
     }),
     concatMap((value: T, index: number) => {
-      if (isLastRequest(value)) {
+      if (requestTracker.shouldProcess(value)) {
         return project(value, index);
       } else {
         return EMPTY;
