84165d4ac35bc0a73e5598a525865ac929e417e6
Simeon Stoykov <simstoykov@google.com>
2020-11-06 15:13:00 +0000

    Removed the builders, refactored tests

--- grr/server/grr_response_server/gui/ui/components/flow_details/helpers/osquery_results_table_test.ts
+++ grr/server/grr_response_server/gui/ui/components/flow_details/helpers/osquery_results_table_test.ts
@@ -1,8 +1,24 @@
 import { TestBed, async } from '@angular/core/testing';
 import { HelpersModule } from './module';
-import { OsqueryTableBuilder, ParsedDetailsTable } from './osquery_test_utils';
 import { DebugElement } from '@angular/core';
 import { OsqueryResultsTable } from './osquery_results_table';
+import { By } from '@angular/platform-browser';
+import { OsqueryTable } from '@app/lib/api/api_interfaces';
+import { newOsqueryTable } from '../plugins/osquery_details_test';
+
+/** Helper data structure to parse an osquery_results_table */
+class OsqueryResultsTableDOM {
+  queryDiv = this.rootElement.query(By.css('.results-query-text'));
+  queryText = this.queryDiv.nativeElement.innerText;
+
+  columnElements = this.rootElement.queryAll(By.css('th'));
+  columnsText = this.columnElements.map(columnElement => columnElement.nativeElement.innerText);
+
+  cellDivs = this.rootElement.queryAll(By.css('td'));
+  cellsText = this.cellDivs.map(cellDiv => cellDiv.nativeElement.innerText);
+
+  constructor(private readonly rootElement: DebugElement) { }
+}
 
 describe('OsqueryResultsTable Component', () => {
   beforeEach(async(() => {
@@ -17,51 +33,40 @@ describe('OsqueryResultsTable Component', () => {
 
   /**
    * Function that creates a component fixture which is supplied with the
-   * OsqueryTable values provided by the builder
+   * OsqueryTable values provided
    */
-  function createElementFrom(osqueryTableBuilder: OsqueryTableBuilder)
+  function createElementFrom(osqueryTable: OsqueryTable)
     : DebugElement {
     const fixture = TestBed.createComponent(OsqueryResultsTable);
 
-    fixture.componentInstance.tableToDisplay = osqueryTableBuilder.build();
+    fixture.componentInstance.tableToDisplay = osqueryTable;
     fixture.detectChanges();
 
     return fixture.debugElement;
   }
 
-  it('should display a table with 5 rows and 5 columns', () => {
+  it('should display a table with 5 rows and 5 columns, together with the query', () => {
     const rowNumber = 5;
     const columnNumber = 5;
+    const query = 'hyd';
 
-    const columns = Array(columnNumber).fill(0).map((_v, index) => `Column ${index}`);
-    const values = Array(rowNumber).fill(0).map((_v1, row) => {
-      return Array(columnNumber).fill(0).map((_v2, col) => `row-${row}, col-${col}`);
+    const columns = Array(columnNumber).fill(0).map((_, index) => `Column ${index}`);
+    const values = Array(rowNumber).fill(0).map((_, row) => {
+      return Array(columnNumber).fill(0).map((_2, col) => `row-${row}, col-${col}`);
     });
 
-    const tableBuilder = new OsqueryTableBuilder()
-      .withColumns(columns)
-      .withValues(values);
+    const table = newOsqueryTable(query, columns, values);
 
-    const osqueryResultsTable = createElementFrom(tableBuilder);
-    const parsedTable = new ParsedDetailsTable(osqueryResultsTable);
+    const osqueryResultsTable = createElementFrom(table);
+    const parsedTable = new OsqueryResultsTableDOM(osqueryResultsTable);
 
     expect(parsedTable.columnElements.length).toEqual(columnNumber);
     expect(parsedTable.columnsText).toEqual(columns);
 
     expect(parsedTable.cellDivs.length).toEqual(rowNumber * columnNumber);
     expect(parsedTable.cellsText).toEqual(values.flat());
-  });
-
-  it('should display query text', () => {
-    const query = 'hyd';
-
-    const tableBuilder = new OsqueryTableBuilder()
-      .withQuery(query);
-
-    const osqueryResultsTable = createElementFrom(tableBuilder);
-    const parsedTable = new ParsedDetailsTable(osqueryResultsTable);
 
     expect(parsedTable.queryDiv).toBeTruthy();
     expect(parsedTable.queryText).toEqual(query);
-  })
+  });
 });
--- grr/server/grr_response_server/gui/ui/components/flow_details/plugins/osquery_details_test.ts
+++ grr/server/grr_response_server/gui/ui/components/flow_details/plugins/osquery_details_test.ts
@@ -1,14 +1,77 @@
-import {async, TestBed, ComponentFixture} from '@angular/core/testing';
-import {initTestEnvironment} from '@app/testing';
+import { async, TestBed, ComponentFixture} from '@angular/core/testing';
+import { initTestEnvironment } from '@app/testing';
 
-import {PluginsModule} from './module';
+import { PluginsModule } from './module';
 import { OsqueryDetails } from './osquery_details';
-import { FlowState } from '@app/lib/models/flow';
-import { OsqueryFlowListEntryBuilder, ParsedOsqueryDetails } from '../helpers/osquery_test_utils';
+import { FlowState, FlowListEntry } from '@app/lib/models/flow';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { newFlowListEntry, newFlowResultSet, newFlow } from '@app/lib/models/model_test_util';
 
 initTestEnvironment();
 
-fdescribe('osquery-details component', () => {
+/**
+ * Builds an OsqueryTable
+ * @param query The Osquery query which produced this table
+ * @param columns Column names of the table
+ * @param rows Array of arrays containing row values
+ */
+export function newOsqueryTable(
+  query: string,
+  columns: ReadonlyArray<string>,
+  rows: ReadonlyArray<ReadonlyArray<string>>,
+) {
+  return {
+    query,
+    header: {
+      columns: columns.map(colName => ({name: colName})),
+    },
+    rows: rows.map(rowValues => ({values: rowValues})),
+  };
+}
+
+/** Helper data structure to parse and expose all elements of interest from the OsqueryDetails DOM */
+class OsqueryDetailsDOM {
+  inProgressDiv = this.rootElement?.query(By.css('.in-progress'));
+  inProgressText = this.inProgressDiv?.nativeElement.innerText;
+
+  errorDiv = this.rootElement?.query(By.css('.error'));
+  stdErrDiv = this.errorDiv?.query(By.css('div'));
+  stdErrText = this.stdErrDiv?.nativeElement.innerText;
+
+  resultsTableDiv = this.rootElement.query(By.css('.results-table'));
+
+  progressTableDiv = this.rootElement.query(By.css('.progress-table'));
+
+  showAdditionalDiv = this.progressTableDiv?.query(By.css('.show-additional'));
+  showAdditionalButton = this.showAdditionalDiv?.query(By.css('button'));
+  showAdditionalButtonText = this.showAdditionalButton?.nativeElement.textContent;
+
+  errorAdditionalDiv = this.rootElement.query(By.css('.error-show-additional'));
+
+  errorAdditionalSpan = this.errorAdditionalDiv?.query(By.css('span'));
+  errorAdditionalSpanText = this.errorAdditionalSpan?.nativeElement.innerText;
+
+  errorAdditionalButton = this.errorAdditionalDiv?.query(By.css('button'));
+  errorAdditionalButtonText = this.errorAdditionalButton?.nativeElement.textContent;
+
+  constructor(private readonly rootElement: DebugElement) { }
+}
+
+/**
+ * Function that creates a component fixture which is supplied with the
+ * FlowListEntry value provided
+ */
+function createFixtureFrom(flowListEntry: FlowListEntry)
+  : ComponentFixture<OsqueryDetails> {
+  const fixture = TestBed.createComponent(OsqueryDetails);
+  fixture.componentInstance.flowListEntry = flowListEntry;
+  fixture.detectChanges();
+
+  return fixture;
+}
+
+describe('osquery-details component', () => {
   beforeEach(async(() => {
     TestBed
         .configureTestingModule({
@@ -21,28 +84,19 @@ fdescribe('osquery-details component', () => {
         .compileComponents();
   }));
 
-  /**
-   * Function that creates a component fixture which is supplied with the
-   * FlowListEntry values provided by the builder
-   */
-  function createFixtureFrom(flowListEntryBuilder: OsqueryFlowListEntryBuilder)
-    : ComponentFixture<OsqueryDetails> {
-    const fixture = TestBed.createComponent(OsqueryDetails);
-    fixture.componentInstance.flowListEntry = flowListEntryBuilder.build();
-    fixture.detectChanges();
-
-    return fixture;
-  }
-
   it('should display only the query argument when flow is still running', () => {
     const testQuery = 'SELECT * FROM users LIMIT 10;';
-    const testFlowListEntry = new OsqueryFlowListEntryBuilder()
-      .withFlowState(FlowState.RUNNING)
-      .withQuery(testQuery);
+    const testFlowListEntry = newFlowListEntry({
+      state: FlowState.RUNNING,
+      args: {
+        query: testQuery,
+      },
+    });
+
     const expectedQueryText = `Query in progress: ${testQuery}`;
 
     const fixture = createFixtureFrom(testFlowListEntry);
-    const parsedElements = new ParsedOsqueryDetails(fixture.debugElement);
+    const parsedElements = new OsqueryDetailsDOM(fixture.debugElement);
 
     expect(parsedElements.resultsTableDiv).toBeFalsy();
     expect(parsedElements.progressTableDiv).toBeFalsy();
@@ -57,12 +111,20 @@ fdescribe('osquery-details component', () => {
   it('should display only the stderr error if the flow encounters an error', () => {
     const testStderr = 'just a standard err';
     const exptectedText = `stderr is: ${testStderr}`;
-    const testFlowListEntry = new OsqueryFlowListEntryBuilder()
-      .withFlowState(FlowState.ERROR)
-      .withStderr(testStderr);
+
+    const testFlowListEntry = {
+      flow: newFlow({
+        state: FlowState.ERROR,
+      }),
+      resultSets: [
+        newFlowResultSet({
+          stderr: testStderr,
+        }),
+      ],
+    };
 
     const fixture = createFixtureFrom(testFlowListEntry);
-    const parsedElements = new ParsedOsqueryDetails(fixture.debugElement);
+    const parsedElements = new OsqueryDetailsDOM(fixture.debugElement);
 
     expect(parsedElements.inProgressDiv).toBeFalsy();
     expect(parsedElements.resultsTableDiv).toBeFalsy();
@@ -80,14 +142,19 @@ fdescribe('osquery-details component', () => {
     const testColName = 'column';
     const testCellValue = 'grr!';
 
-    const testFlowListEntry = new OsqueryFlowListEntryBuilder()
-      .withFlowState(FlowState.FINISHED)
-      .withProgressTable([testColName], [[testCellValue]])
-      .withProgressRowsCount(1) // Not more than the number of rows in the progress table(1)
-      .withQuery(testQuery);
+    const testFlowListEntry = newFlowListEntry({
+      state: FlowState.FINISHED,
+      progress: {
+        partialTable: newOsqueryTable(testQuery, [testColName], [[testCellValue]]),
+        totalRowCount: 1, // Not more than the number of rows in the progress table (1),
+      },
+      args: {
+        query: testQuery,
+      }
+    });
 
     const fixture = createFixtureFrom(testFlowListEntry);
-    const parsedElements = new ParsedOsqueryDetails(fixture.debugElement);
+    const parsedElements = new OsqueryDetailsDOM(fixture.debugElement);
 
     expect(parsedElements.inProgressDiv).toBeFalsy();
     expect(parsedElements.errorDiv).toBeFalsy();
@@ -104,14 +171,19 @@ fdescribe('osquery-details component', () => {
     const testColName = 'column';
     const testCellValue = 'grr!';
 
-    const testFlowListEntry = new OsqueryFlowListEntryBuilder()
-      .withFlowState(FlowState.FINISHED)
-      .withProgressTable([testColName], [[testCellValue]])
-      .withProgressRowsCount(2) // More than the number of rows in the progress table (1)
-      .withQuery(testQuery);
+    const testFlowListEntry = newFlowListEntry({
+      state: FlowState.FINISHED,
+      progress: {
+        partialTable: newOsqueryTable(testQuery, [testColName], [[testCellValue]]),
+        totalRowCount: 2, // More than the number of rows in the progress table (1),
+      },
+      args: {
+        query: testQuery,
+      }
+    });
 
     const fixture = createFixtureFrom(testFlowListEntry);
-    const parsedElements = new ParsedOsqueryDetails(fixture.debugElement);
+    const parsedElements = new OsqueryDetailsDOM(fixture.debugElement);
 
     expect(parsedElements.inProgressDiv).toBeFalsy();
     expect(parsedElements.errorDiv).toBeFalsy();
@@ -126,21 +198,27 @@ fdescribe('osquery-details component', () => {
   });
 
   it('shouldn\'t display the show-additional section if flow is still in progress', () => {
-    const testFlowListEntry = new OsqueryFlowListEntryBuilder()
-      .withFlowState(FlowState.RUNNING);
+    const testQuery = 'grr?';
+    const testFlowListEntry = newFlowListEntry({
+      state: FlowState.RUNNING,
+      args: {
+        query: testQuery,
+      }
+    });
 
     const fixture = createFixtureFrom(testFlowListEntry);
-    const parsedElements = new ParsedOsqueryDetails(fixture.debugElement);
+    const parsedElements = new OsqueryDetailsDOM(fixture.debugElement);
 
     expect(parsedElements.showAdditionalDiv).toBeFalsy();
   });
 
   it('should display error-show-additional section if result and progress are undefined (for some reason)', () => {
-    const testFlowListEntry = new OsqueryFlowListEntryBuilder()
-      .withFlowState(FlowState.FINISHED);
+    const testFlowListEntry = newFlowListEntry({
+      state: FlowState.FINISHED,
+    });
 
     const fixture = createFixtureFrom(testFlowListEntry);
-    const parsedElements = new ParsedOsqueryDetails(fixture.debugElement);
+    const parsedElements = new OsqueryDetailsDOM(fixture.debugElement);
 
     expect(parsedElements.errorAdditionalDiv).toBeTruthy();
     expect(parsedElements.errorAdditionalSpan).toBeTruthy();
