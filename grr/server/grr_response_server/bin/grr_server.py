80830a2a8c3a2e975d953cc59cb16383476ecaa2
Gilad Tsehori <giladt@google.com>
2020-11-16 11:33:48 +0000

    Remove added whitespace

--- grr/server/grr_response_server/bin/grr_server.py
+++ grr/server/grr_response_server/bin/grr_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Lint as: python3
 """This is a single binary that runs all the GRR components.
+
 To use this entry point you must run "grr_config_updater initialize" first.
 """
 from __future__ import absolute_import
