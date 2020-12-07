7222d9b8f2e05f61002e1ccfda706ab35ccec8a4
Simeon Stoykov <simstoykov@google.com>
2020-12-07 11:03:23 +0000

    Forgot to remove feature functionality

--- grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
+++ grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
@@ -5,8 +5,6 @@ import {shareReplay} from 'rxjs/operators';
 import {MatDialog} from '@angular/material/dialog';
 
 import {OsqueryArgs} from '../../lib/api/api_interfaces';
-import {OsqueryQueryHelper} from './osquery_query_helper/osquery_query_helper';
-import {isNonNull} from '@app/lib/preconditions';
 
 
 /** Form that configures an Osquery flow. */
@@ -36,13 +34,7 @@ export class OsqueryForm extends FlowArgumentForm<OsqueryArgs> implements
   }
 
   browseTablesClicked(): void {
-    const openedDialog = this.dialog.open(OsqueryQueryHelper);
-
-    openedDialog.afterClosed().subscribe(newQueryReceived => {
-      if (isNonNull(newQueryReceived)) {
-        this.overwriteQuery(newQueryReceived);
-      }
-    }); // No need to unsubscribe as it completes when the dialog is closed.
+    throw Error('Not implemented yet.');
   }
 
   ngOnInit(): void {
@@ -51,11 +43,5 @@ export class OsqueryForm extends FlowArgumentForm<OsqueryArgs> implements
 
   openSettings() {
     this.settingsShown = true;
-  }
-
-  private overwriteQuery(newValue: string): void {
-    this.form.patchValue({
-      query: newValue,
-    });
   }
 }
