f0d3216d52924671ac02afd1d97127d1e52f68da
Simeon Stoykov <35629151+simstoykov@users.noreply.github.com>
2020-12-11 11:19:33 +0100

    Upgrade editor and prepare for the osquery helper (#899)
    * Added mat-form-field capability to the editor; Changed editor theme; Hid low-level settings
    
    * Forgot to remove feature functionality
    
    * Merged classes together; Fixed get id which changed on every access
    
    * Added tests, fixed a bug if the editor value is undefined, changed import method because the compiler complined

--- grr/server/grr_response_server/gui/ui/components/code_editor/code_editor.ts
+++ grr/server/grr_response_server/gui/ui/components/code_editor/code_editor.ts
@@ -7,8 +7,15 @@ import {
     ViewEncapsulation,
     Input,
     forwardRef,
+    OnDestroy,
+    HostBinding,
 } from '@angular/core';
-import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import {NG_VALUE_ACCESSOR, ControlValueAccessor, NgControl} from '@angular/forms';
+import {MatFormFieldControl} from '@angular/material/form-field';
+import {Subject} from 'rxjs';
+import {FocusMonitor} from '@angular/cdk/a11y';
+import {takeUntil} from 'rxjs/operators';
+import {isNonNull} from '@app/lib/preconditions';
 
 import * as CodeMirror from 'codemirror';
 // importing sql-hint is needed for SQL syntax highlighting
@@ -18,28 +25,13 @@ import 'codemirror/addon/hint/show-hint.js';
 
 type OnChangeFn = (textValue: string) => void;
 
-/** Displays a code editor. */
-@Component({
-  selector: 'code-editor',
-  templateUrl: './code_editor.ng.html',
-  styleUrls: ['./code_editor.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => CodeEditor),
-      multi: true,
-    },
-  ],
-})
-export class CodeEditor implements AfterViewInit, ControlValueAccessor {
+/** The core functionality of the code editor */
+class CodeEditorCore implements AfterViewInit, ControlValueAccessor {
   @ViewChild('editorTarget')
   private readonly editorTarget!: ElementRef;
-  private editor?: CodeMirror.Editor;
+  protected editor?: CodeMirror.Editor;
 
   private latestOverwrite = '';
-  @Input()
   set editorValue(newValue: string) {
     // The editor is initialized in ngAfterViewInit, and will be undefined
     // before that, including when @Input() arguments are set.
@@ -50,11 +42,12 @@ export class CodeEditor implements AfterViewInit, ControlValueAccessor {
   get editorValue() {
     return this.editor?.getValue() ?? '';
   }
+  protected readonly editorValueChanges$ = new Subject<string>();
 
-  // ControlValueAccessor functionality for Angular Forms interoperability
-  announceValueChanged: OnChangeFn = () => { };
-  writeValue(value: string): void {
-    this.editorValue = value;
+  // ControlValueAccessor functionality
+  private announceValueChanged: OnChangeFn = () => { };
+  writeValue(value: string | undefined | null): void {
+    this.editorValue = value ?? '';
   }
   registerOnChange(fn: OnChangeFn): void {
     this.announceValueChanged = fn;
@@ -74,7 +67,7 @@ export class CodeEditor implements AfterViewInit, ControlValueAccessor {
     this.editor = CodeMirror.fromTextArea(this.editorTarget.nativeElement, {
       value: '',
       mode: 'text/x-sqlite',
-      theme: 'idea',
+      theme: 'neo',
       extraKeys: {'Ctrl-Space': 'autocomplete'},
       lineNumbers: true,
       lineWrapping: true,
@@ -82,6 +75,124 @@ export class CodeEditor implements AfterViewInit, ControlValueAccessor {
 
     this.editor.on('change', () => {
       this.announceValueChanged(this.editorValue);
+      this.editorValueChanges$.next(this.editorValue);
     });
+  }
+}
+
+/**
+ * Displays a code editor.
+ * It can be used as an Angular form field, and it can be put inside
+ * <mat-form-field></mat-form-field> tags.
+ *
+ * @see {@link CodeEditorCore}
+ */
+@Component({
+  selector: 'code-editor',
+  templateUrl: './code_editor.ng.html',
+  styleUrls: ['./code_editor.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CodeEditor),
+      multi: true,
+    },
+    {
+      provide: MatFormFieldControl,
+      useExisting: CodeEditor,
+      multi: true,
+    }
+  ],
+})
+export class CodeEditor extends CodeEditorCore
+    implements MatFormFieldControl<string>, OnDestroy {
+  private static uniqueNumber = 0;
+
+  private readonly unsubscribe$ = new Subject<void>();
+
+  readonly controlType = 'code-editor';
+
+  /**
+   * ID to associate all labels and hints of the enclosing mat-form-field with.
+   */
+  @HostBinding()
+  readonly id: string;
+
+  get value() {
+    return this.editorValue;
+  }
+  set value(newValue: string | null) {
+    this.editorValue = newValue ?? '';
+  }
+
+  get empty(): boolean {
+    return this.editorValue === '';
+  }
+
+  readonly stateChanges = new Subject<void>();
+  focused = false;
+
+  // not implemented
+  shouldLabelFloat = true;
+
+  // not implemented
+  required = false;
+
+  // not implemented
+  disabled = false;
+
+  // not implemented
+  placeholder = '';
+
+  // not implemented
+  ngControl: NgControl | null = null;
+
+  // not implemented
+  errorState = false;
+
+  /**
+   * Whether the code editor input will be focused when the enclosing
+   * mat-form-field container is clicked
+   */
+  @Input()
+  focusOnContainerClick = true;
+  onContainerClick(event: MouseEvent): void {
+    if (this.focusOnContainerClick) {
+      this.editor?.focus();
+    }
+  }
+
+  // not implemented
+  setDescribedByIds(ids: string[]): void { }
+
+  constructor(
+      private focusMonitor: FocusMonitor,
+      private rootElement: ElementRef<HTMLElement>,
+  ) {
+    super();
+
+    this.id = `${this.controlType}-${CodeEditor.uniqueNumber}`;
+    CodeEditor.uniqueNumber += 1;
+
+    focusMonitor.monitor(rootElement.nativeElement, true).subscribe(
+        focused => {
+          this.focused = isNonNull(focused);
+          this.stateChanges.next();
+        });
+
+    this.editorValueChanges$.pipe(
+        takeUntil(this.unsubscribe$)
+      ).subscribe(() => {
+        this.stateChanges.next();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.focusMonitor.stopMonitoring(this.rootElement.nativeElement);
+
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
--- grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
+++ grr/server/grr_response_server/gui/ui/components/flow_args_form/osquery_form.ts
@@ -2,6 +2,7 @@ import {ChangeDetectionStrategy, Component, OnInit, Output} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {FlowArgumentForm} from '@app/components/flow_args_form/form_interface';
 import {shareReplay} from 'rxjs/operators';
+import {MatDialog} from '@angular/material/dialog';
 
 import {OsqueryArgs} from '../../lib/api/api_interfaces';
 
@@ -23,10 +24,24 @@ export class OsqueryForm extends FlowArgumentForm<OsqueryArgs> implements
     ignoreStderrErrors: new FormControl(null),
   });
 
+  settingsShown = false;
+
   @Output() readonly formValues$ = this.form.valueChanges.pipe(shareReplay(1));
   @Output() readonly status$ = this.form.statusChanges.pipe(shareReplay(1));
 
+  constructor(private readonly dialog: MatDialog) {
+    super();
+  }
+
+  browseTablesClicked(): void {
+    throw Error('Not implemented yet.');
+  }
+
   ngOnInit(): void {
     this.form.patchValue(this.defaultFlowArgs);
+  }
+
+  openSettings() {
+    this.settingsShown = true;
   }
 }
