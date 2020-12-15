987cff7ffc6c3a4640fd6b37cef2a172831a577f
Simeon Stoykov <simstoykov@google.com>
2020-12-15 19:46:08 +0000

    Added test for the menu items with harnesses

--- grr/server/grr_response_server/gui/ui/components/expandable_hash/expandable_hash_test.ts
+++ grr/server/grr_response_server/gui/ui/components/expandable_hash/expandable_hash_test.ts
@@ -1,28 +1,21 @@
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
-
-import {ExpandableHash} from './expandable_hash';
-import {initTestEnvironment} from '@app/testing';
-
 import {By} from '@angular/platform-browser';
 import {Hash} from '@app/lib/api/api_interfaces';
-import {DebugElement} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {ClipboardModule} from '@angular/cdk/clipboard';
 import {MatIconModule} from '@angular/material/icon';
-
-import {HarnessLoader} from '@angular/cdk/testing';
 import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
 import {MatMenuHarness} from '@angular/material/menu/testing';
 import {MatButtonHarness} from '@angular/material/button/testing';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+import {initTestEnvironment} from '@app/testing';
+import {ExpandableHash} from './expandable_hash';
 
 initTestEnvironment();
 
 describe('ExpandableHash component', () => {
-  let component: ExpandableHash;
-  let fixture: ComponentFixture<ExpandableHash>;
-  let harnessLoader: HarnessLoader;
-
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ExpandableHash ],
@@ -31,32 +24,38 @@ describe('ExpandableHash component', () => {
         MatMenuModule,
         MatButtonModule,
         MatIconModule,
+        NoopAnimationsModule,
       ],
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ExpandableHash);
-    component = fixture.componentInstance;
-    harnessLoader = TestbedHarnessEnvironment.loader(fixture);
-    fixture.detectChanges();
-  });
-
   class ExpandableHashDOM {
-    expandButton = this.root.query(By.css('.button-expand-expandable-hash-class'));
-    expandButtonText = this.expandButton?.nativeElement.innerText;
+    harnessLoader = TestbedHarnessEnvironment.loader(this.rootFixture);
 
-    noHashesSpan = this.root.query(By.css('.no-hashes-expandable-hash-class'));
+    expandButton = this.rootFixture.debugElement.query(
+        By.css('.button-expand-expandable-hash-class'));
+    expandButtonText = this.expandButton?.nativeElement.innerText;
+    expandButtonHarness = this.harnessLoader.getHarness<MatButtonHarness>(
+        MatButtonHarness,
+    );
+
+    noHashesSpan = this.rootFixture.debugElement.query(
+        By.css('.no-hashes-expandable-hash-class'));
     noHashesSpanText = this.noHashesSpan?.nativeElement.innerText;
 
-    constructor(readonly root: DebugElement) { }
+    menuHarness = this.harnessLoader.getHarness<MatMenuHarness>(
+      MatMenuHarness.with({triggerText: 'Copy value'}));
+
+    constructor(readonly rootFixture: ComponentFixture<ExpandableHash>) { }
   }
 
   function initComponentWithHashes(hashes: Hash): ExpandableHashDOM {
-    component.hashes = hashes;
+    const fixture = TestBed.createComponent(ExpandableHash);
+    fixture.componentInstance.hashes = hashes;
     fixture.detectChanges();
-    return new ExpandableHashDOM(fixture.debugElement);
+
+    return new ExpandableHashDOM(fixture);
   }
 
   it('should only display an en dash (–) if no hashes are available', () => {
@@ -84,17 +83,35 @@ describe('ExpandableHash component', () => {
     expect(dom.expandButtonText).toBe('Copy value');
   });
 
-  fit('should display all three hashes and a copy button if all hashes are',
+  it('should display all hashes and copy-all item if all are available',
       async () => {
-        const matButton  = await harnessLoader.getHarness<MatButtonHarness>(
-            MatButtonHarness.with(),
-        );
-        await matButton.click();
+        const all3Hashes = {
+          sha256: 'sha256value',
+          sha1: 'sha1value',
+          md5: 'md5value',
+        };
+        const dom = initComponentWithHashes(all3Hashes);
 
-        const expandedMenu = await harnessLoader.getHarness<MatMenuHarness>(
-            MatMenuHarness.with({triggerText: 'Copy value'}));
-
+        const expandButton  = await dom.expandButtonHarness;
+        const expandedMenu = await dom.menuHarness;
+        await expandButton.click();
         const menuItems = await expandedMenu.getItems();
+
         expect(menuItems.length).toBe(4);
+
+        const textOfSha256 = await menuItems[0].getText();
+        expect(textOfSha256).toContain('SHA-256');
+        expect(textOfSha256).toContain('sha256value');
+
+        const textOfSha1 = await menuItems[1].getText();
+        expect(textOfSha1).toContain('SHA-1');
+        expect(textOfSha1).toContain('sha1value');
+
+        const textOfMd5 = await menuItems[2].getText();
+        expect(textOfMd5).toContain('MD5');
+        expect(textOfMd5).toContain('md5value');
+
+        const textOfCopyAll = await menuItems[3].getText();
+        expect(textOfCopyAll).toContain('All hash information');
       });
 });
--- grr/server/grr_response_server/gui/ui/components/expandable_hash/module.ts
+++ grr/server/grr_response_server/gui/ui/components/expandable_hash/module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ExpandableHash } from './expandable_hash'
-import { ClipboardModule } from '@angular/cdk/clipboard';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ExpandableHash} from './expandable_hash'
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
