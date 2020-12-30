01754f0f5440f0c483fdd1aa52203fc2f7f68071
mol123 <60134413+mol123@users.noreply.github.com>
2020-12-30 17:21:46 +0100

    Fix travis server build.

--- travis/build_local_pyindex.sh
+++ travis/build_local_pyindex.sh
@@ -66,6 +66,9 @@ function download_packages() {
   # if the version of idna needed by requests is not present.
   # See https://ci.appveyor.com/project/grr/grr/builds/20793753.
   pip download --find-links=local_pypi --dest=local_pypi idna==2.7
+
+  # This is needed to build some of the wheels.
+  pip download --find-links=local_pypi --dest=local_pypi wheel setuptools
 }
 
 function verify_packages() {
