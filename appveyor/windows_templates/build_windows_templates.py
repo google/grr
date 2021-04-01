eba5227ab09c2f016f93b1ec168f166327676347
Łukasz Hanuszczak <1961136+panhania@users.noreply.github.com>
2021-04-01 15:15:13 +0200

    Add pip debug information to the AppVeyor script.

--- appveyor/windows_templates/build_windows_templates.py
+++ appveyor/windows_templates/build_windows_templates.py
@@ -165,6 +165,9 @@ class WindowsTemplateBuilder(object):
       subprocess.check_call([self.virtualenv_python32] + cmd +
                             ["--upgrade", "pip>=8.1.1"])
 
+    subprocess.check_call([self.pip32, "debug", "--verbose"])
+    subprocess.check_call([self.pip64, "debug", "--verbose"])
+
   def GitCheckoutGRR(self):
     os.chdir(args.build_dir)
     subprocess.check_call(
