9466152cfe8cda52e5678e131cc3e63c4dc0970b
Christian Clauss <cclauss@me.com>
2021-07-15 11:18:22 +0200

    global py_args to avoid using linter directive

--- grr/core/executables/python_hacks/shutdown_host.py
+++ grr/core/executables/python_hacks/shutdown_host.py
@@ -4,25 +4,30 @@
 
 import platform
 
-tested_versions = ['xp', 'vista', '2008', '2003']
-cmd = 'cmd'
-args = ['/c', '%SystemRoot%\\System32\\shutdown.exe', '/s', '/f']
-os_version = platform.platform().lower()
+def main():
+  global py_args  # Predefined in the environment. See google/grr#937
+  tested_versions = ['xp', 'vista', '2008', '2003']
+  cmd = 'cmd'
+  args = ['/c', '%SystemRoot%\\System32\\shutdown.exe', '/s', '/f']
+  os_version = platform.platform().lower()
 
-# pylint: disable=undefined-variable
-if 'time_in_seconds' in py_args:
-  args.extend(['/t', py_args['time_in_seconds']])
-else:
-  args.extend(['/t', '20'])
+  if 'time_in_seconds' in py_args:
+    args.extend(['/t', py_args['time_in_seconds']])
+  else:
+    args.extend(['/t', '20'])
 
-if 'reason' in py_args:
-  args.extend(['/c', py_args['reason']])
+  if 'reason' in py_args:
+    args.extend(['/c', py_args['reason']])
 
-for version in tested_versions:
-  if os_version.find(version) != -1:
-    stdout, stderr, exit_status, time_taken = client_utils_common.Execute(
-        cmd, args, time_limit=-1, bypass_allowlist=True)
-    magic_return_str = '%s, %s, %s, %s' % (stdout.encode('base64'),
-                                           stderr.encode('base64'), exit_status,
-                                           time_taken)
-    break
+  for version in tested_versions:
+    if os_version.find(version) != -1:
+      stdout, stderr, exit_status, time_taken = client_utils_common.Execute(
+          cmd, args, time_limit=-1, bypass_allowlist=True)
+      magic_return_str = '%s, %s, %s, %s' % (stdout.encode('base64'),
+                                             stderr.encode('base64'), exit_status,
+                                             time_taken)
+      break
+
+
+if __name__ == '__main__':
+  main()
