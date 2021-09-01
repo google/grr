158e4fb796ad1b1706b920f22beccd9912969e7b
Max Vogler <max.vogler@gmail.com>
2021-09-01 15:46:30 +0200

    Fix install_mem_usage_cron.

--- appveyor/e2e_tests/install_mem_usage_cron.sh
+++ appveyor/e2e_tests/install_mem_usage_cron.sh
@@ -7,5 +7,5 @@ set -ex
 # Write header row of log file.
 echo "$(date) $(free -hmw | grep available)" >> /var/log/grr_e2e_mem_usage.log
 # Install the crontab file.
-cp "${APPVEYOR_BUILD_FOLDER}/appveyor/e2e_tests/grr_e2e_mem_usage" /etc/cron.d/
+cp "appveyor/e2e_tests/grr_e2e_mem_usage" /etc/cron.d/
 systemctl restart cron
