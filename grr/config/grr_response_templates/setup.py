f3610994285ed64df9b2bb3f861de0cd2ffd5f58
mbushkov <realbushman@gmail.com>
2023-02-28 19:27:25 +0100

    Added ubuntu version-specific deb packages to required templates list.

--- grr/config/grr_response_templates/setup.py
+++ grr/config/grr_response_templates/setup.py
@@ -39,7 +39,8 @@ class Sdist(sdist):
 
   REQUIRED_TEMPLATES = [
       "GRR_maj.minor_amd64.msi.zip",
-      "grr_maj.minor_amd64.deb.zip",
+      "grr_ubuntu18_maj.minor_amd64.deb.zip",
+      "grr_ubuntu22_maj.minor_amd64.deb.zip",
       "grr_maj.minor_amd64.xar.zip",
       "grr_maj.minor_amd64.rpm.zip",
   ]
