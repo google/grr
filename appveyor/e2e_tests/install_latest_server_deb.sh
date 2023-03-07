0418cfde6716f63a22324af4263f57442c235d9a
mbushkov <realbushman@gmail.com>
2023-03-07 21:36:04 +0100

    Adjusting logging.

--- appveyor/e2e_tests/install_latest_server_deb.sh
+++ appveyor/e2e_tests/install_latest_server_deb.sh
@@ -32,7 +32,12 @@ DEBIAN_FRONTEND=noninteractive apt install -y "$GITHUB_WORKSPACE"/_artifacts/grr
 mysql -u root --password=root -e "CREATE USER 'runner'@'localhost' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES on grr.* to runner@'localhost'; GRANT ALL PRIVILEGES on fleetspeak.* to runner@'localhost'; CREATE DATABASE fleetspeak"
 
 grr_config_updater initialize --noprompt --external_hostname=localhost --admin_password=e2e_tests --mysql_username=runner --mysql_password=password --mysql_host=localhost --mysql_db=grr --mysql_fleetspeak_db=fleetspeak --use_fleetspeak
+
 echo 'Logging.verbose: True' >> /etc/grr/server.local.yaml
+
+mkdir -p /var/log/grr
+echo 'Logging.path: /var/log/grr' >> /etc/grr/server.local.yaml
+
 systemctl restart grr-server
 
 echo "Installation of server deb completed."
