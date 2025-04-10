37f5c227834c743354e29b438d58a7fe80168e86
Dan Aschwanden <daschwanden@google.com>
2025-04-10 11:09:07 +0000

    Adds test case

--- grr/server/grr_response_server/output_plugins/gcs_plugin.py
+++ grr/server/grr_response_server/output_plugins/gcs_plugin.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""GCS live output plugin."""
+"""GCS output plugin for FileFinder flow results."""
 
 import jinja2
 import json
@@ -30,13 +30,14 @@ def _ToDict(rdfval: rdf_structs.RDFProtoStruct) -> JsonDict:
     return json_format.MessageToDict(rdfval.AsPrimitiveProto(), float_precision=8)
 
 
-def upload_blob_from_stream(project_id, bucket_name, file_obj, client_id, flow_id, destination_blob_name):
+def upload_blob_from_stream(project_id, bucket_name, client_path, client_id, flow_id, destination_blob_name):
     """Uploads bytes from a stream to a blob"""
     storage_client = storage.Client(project=project_id)
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(client_id+"/"+flow_id+destination_blob_name)
-    file_obj.seek(0)
-    blob.upload_from_file(file_obj)
+    fd = file_store.OpenFile(client_path)
+    fd.seek(0)
+    blob.upload_from_file(fd)
 
 
 class GcsOutputPlugin(output_plugin.OutputPlugin):
@@ -53,27 +54,22 @@ class GcsOutputPlugin(output_plugin.OutputPlugin):
     client_id = response.client_id
     flow_id = response.flow_id
 
-    logging.error("--------------------------------------------------")
-    logging.error("--------------- response client_id: %s", client_id)
     if response.HasField("payload"):
-        logging.error("--------------- response payload: %s", _ToDict(response.payload))
         if response.payload.HasField("transferred_file"):
-            logging.error("--------------- response transferredFile: %s", _ToDict(response.payload.transferred_file))
             if response.payload.stat_entry.st_size > 0 :
-                logging.error("++++++ read file, pushing to GCS")
                 client_path = db.ClientPath.FromPathSpec(client_id, response.payload.stat_entry.pathspec)
-                fd = file_store.OpenFile(client_path)
-                upload_blob_from_stream(self.args.project_id, self.args.gcs_bucket, fd, client_id, flow_id, response.payload.stat_entry.pathspec.path)
+                upload_blob_from_stream(self.args.project_id, self.args.gcs_bucket, client_path, client_id, flow_id, response.payload.stat_entry.pathspec.path)
+                logging.info("response client_id: %s, flow_id: %s, transferred_file: %s", client_id, flow_id, response.payload.stat_entry.pathspec.path)
             else:
-                logging.error("++++++ file size is 0, nothing to push to GCS")
+                logging.warning("%s : file size is 0, nothing to push to GCS", response.payload.stat_entry.pathspec.path)
 
 
   def ProcessResponses(self, state, responses):
     if self.args.gcs_bucket == "" or self.args.project_id == "":
-        logging.error("---------------- Both GCS bucket and Project ID must be set")
+        logging.error("both GCS bucket and Project ID must be set")
         return
     else:
-        logging.error("---------------- Project ID is %s, GCS bucket is %s", self.args.project_id, self.args.gcs_bucket)
+        logging.info("Project ID: %s, GCS bucket: %s", self.args.project_id, self.args.gcs_bucket)
 
     for response in responses:
       self.ProcessResponse(state, response)
