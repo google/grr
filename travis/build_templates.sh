4b99160001176f65d9acb4f2293ec22e75871b5a
Susanne Westphal <swestphal@google.com>
2025-12-16 10:21:29 +0000

    Remove upload to GCS

--- travis/build_templates.sh
+++ travis/build_templates.sh
@@ -4,15 +4,15 @@ set -e
 
 source "${HOME}/INSTALL/bin/activate"
 
-grr_client_build build --output gcs_upload_dir
+grr_client_build build --output installers_dir
 
 # We only have one template on linux or OS X so using *.zip is safe here
 grr_client_build \
   --verbose \
   --secondary_configs grr/test/grr_response_test/test_data/dummyconfig.yaml \
   repack \
-  --template gcs_upload_dir/*.zip \
-  --output_dir gcs_upload_dir
+  --template installers_dir/*.zip \
+  --output_dir installers_dir
 
 # We don't install on linux because we're running on travis container
 # infrastructure that doesn't allow for sudo (but has startup time and
@@ -22,5 +22,5 @@ grr_client_build \
 # just waits for the installer to be available in a cloud storage bucket for the
 # build, then installs it.
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  sudo installer -verbose -pkg gcs_upload_dir/*.pkg -target /
+  sudo installer -verbose -pkg installers_dir/*.pkg -target /
 fi
