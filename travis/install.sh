2833b8625dd0a339515210e127edd4c72287acf1
Susanne Westphal <swestphal@google.com>
2026-05-12 11:16:45 +0000

    remove no-use pep517

--- travis/install.sh
+++ travis/install.sh
@@ -29,28 +29,22 @@ unset JAVA_TOOL_OPTIONS
 # Note that because of dependencies, order here is important.
 #
 # Proto package.
-pip install --no-use-pep517 -e grr/proto --progress-bar off
+pip install -e grr/proto --progress-bar off
 
 # Depends on grr-response-proto
-pip install --no-use-pep517 -e grr/core --progress-bar off
+pip install -e grr/core --progress-bar off
 
 # Depends on grr-response-core
-pip install --no-use-pep517 -e grr/client --progress-bar off
-
-# Depends on grr-response-core
-pip install --no-use-pep517 -e api_client/python --progress-bar off
-
-# Depends on grr-response-client
-pip install --no-use-pep517 -e grr/client_builder --progress-bar off
+pip install -e api_client/python --progress-bar off
 
 # Depends on grr-response-client-builder
-pip install --no-use-pep517 -e grr/server --progress-bar off
+pip install -e grr/server --progress-bar off
 
 # Depends on grr-api-client and grr-response-proto
-pip install --no-use-pep517 -e colab --progress-bar off
+pip install -e colab --progress-bar off
 
 # Depends on all other packages
-pip install --no-use-pep517 -e grr/test --progress-bar off
+pip install -e grr/test --progress-bar off
 
 cd grr/proto && python makefile.py && cd -
 cd grr/core/grr_response_core/artifacts && python makefile.py && cd -
